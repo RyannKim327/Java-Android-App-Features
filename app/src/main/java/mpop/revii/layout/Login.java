88521fe743d8a464820fb1539e5dbb10b4f97708
RyannKim327 <rksesgundo123@gmail.com>
2024-02-11 08:50:41 +0800

    02-11-24 08:50

--- app/src/main/java/mpop/revii/layout/Login.java
+++ app/src/main/java/mpop/revii/layout/Login.java
@@ -24,8 +24,27 @@ public class Login extends RelativeLayout {
 		sp = ctx.getSharedPreferences("mpop.revii.login", ctx.MODE_PRIVATE);
 		LinearLayout form = new LinearLayout(ctx);
 		EditText password = new EditText(ctx);
+		float d = 5f;
+		float e = 25f;
+		ShapeDrawable draw = new ShapeDrawable(new RoundRectShape(new float[]{
+			d, d, d, d,
+			d, d, d, d
+		}, null, null))
+		ShapeDrawable pass = new ShapeDrawable(new RoundRectShape(new float[]{
+			e, e, e, e,
+			e, e, e, e
+		}, null, null))
+
+		draw.getPaint().setColor(Color.parse("#333333"));
+		pass.getPaint().setColor(Color.parse("#eeeeee"));
 
 		form.setOrientation(LinearLayout.VERTICAL);
+		form.setBackgroundDrawable(draw);
+
+		password.setPadding(5, 3, 5, 3);
+		pass.setBackgroundDrawable(pass);
+
+		form.addView(pass);
 
 		if(!sp.getString("mpop.revii.login.USER_PASSWORD", "").isEmpty()){
 			new Handler().postDelayed(new Runnable(){
